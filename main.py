09b2bd81b652e05f3132e61b5bb670408c1262a3
sondos <alisondos142@gmail.com>
2026-09-13 17:09:07 +0300

    Add more student information

---
+++ main.py
@@ -0,0 +1,11 @@
+name = "Your Name"
+major = "Artificial Intelligence"
+university = "Your University"
+graduation_year = "2027"
+
+print("Student Profile")
+print("----------------")
+print("Name:", name)
+print("Major:", major)
+print("University:", university)
+print("Graduation Year:", graduation_year)
